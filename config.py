c6af8cdebc6d20ff59b060105f27a6df3479cb74
Bryan <bryanlondres5@gmail.com>
2024-04-02 13:46:44 -0400

    configuration settings and dependencies

---
+++ config.py
@@ -0,0 +1,13 @@
+# Elasticsearch Configuration
+elasticsearch_host = 'localhost'
+elasticsearch_port = 9200
+elasticsearch_index = 'stocksight'
+
+# Twitter API Configuration
+consumer_key = 'YOUR_CONSUMER_KEY'
+consumer_secret = 'YOUR_CONSUMER_SECRET'
+access_token = 'YOUR_ACCESS_TOKEN'
+access_token_secret = 'YOUR_ACCESS_TOKEN_SECRET'
+
+# Stock Symbol example
+stock_symbol = 'TSLA'
